a7b5929ef4aa7227c72a91e5156a588d7f7cb8d7
Kelvin Renan Krause <kelvin.krause@cigam.com.br>
2024-09-12 10:39:15 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+class Program{
+    private static void Main(string[] args)
+    {
+        string mensagemDeBoasVindas = "Seja Bem Vindo";
+        Console.WriteLine(mensagemDeBoasVindas);
+        string texto = Console.ReadLine();
+        Console.WriteLine("* {0} *", texto);
+        Console.WriteLine($"# {texto} #");
+    }
+}
