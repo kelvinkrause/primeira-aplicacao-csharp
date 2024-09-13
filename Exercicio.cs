b7dde819c7c1024ce1f945a4f0fa8ae0ea6b22cb
kelvinkrause <kelvinrkrause@outlook.com>
2024-09-13 09:18:16 -0300

    new file:   Exercicio.cs 	modified:   Program.cs 	modified:   bin/Debug/net6.0/primeiro-programa.dll 	modified:   bin/Debug/net6.0/primeiro-programa.pdb 	modified:   obj/Debug/net6.0/primeiro-programa.csproj.CoreCompileInputs.cache 	modified:   obj/Debug/net6.0/primeiro-programa.dll 	modified:   obj/Debug/net6.0/primeiro-programa.pdb 	modified:   obj/Debug/net6.0/ref/primeiro-programa.dll 	modified:   obj/Debug/net6.0/refint/primeiro-programa.dll 	deleted:    Exercicio.cs 	modified:   Program.cs 	modified:   bin/Debug/net6.0/primeiro-programa.dll 	modified:   bin/Debug/net6.0/primeiro-programa.pdb 	modified:   obj/Debug/net6.0/primeiro-programa.csproj.CoreCompileInputs.cache 	modified:   obj/Debug/net6.0/primeiro-programa.dll 	modified:   obj/Debug/net6.0/primeiro-programa.pdb 	modified:   obj/Debug/net6.0/ref/primeiro-programa.dll 	modified:   obj/Debug/net6.0/refint/primeiro-programa.dll

---
+++ Exercicio.cs
@@ -0,0 +1,44 @@
+using System;
+
+class Exercicio{
+
+    public void Exercicio1(){
+        /*EX1: Criar uma variável chamada notaMedia e atribua um valor inteiro a ela. 
+        Caso seu valor seja maior ou igual a 5, escreva na tela "Nota suficiente para aprovação".*/
+
+        int notaMedia = 6;
+        if(notaMedia >= 5){
+            Console.WriteLine("Nota suficiente para aprovação.");
+        }
+
+    }
+
+    public List<string> Exercicio2(){
+        /*EX2: Criar uma lista de linguagens de programação, com as linguagens C#, Java e JavaScript.*/
+        List<string> linguagemDeProgramacao = new List<string>{"C#", "Java", "JavaScript"};
+        return linguagemDeProgramacao;
+    }
+
+    public void Exercicio3(){
+        List<string> linguagens = Exercicio2().ToList();
+        /*EX3: Exibir o valor "C#" no console, utilizando a lista criada no exercício anterior.*/
+        Console.WriteLine(linguagens[0]);
+    }
+
+    public void Exercicio4(){
+        /*Criar um programa que, dado a entrada de dados de um número n inteiro, a partir do teclado, 
+        exibir a n-ésima posição de uma lista.*/
+        Console.Write("Informe um numero: ");
+        string numeroString = Console.ReadLine()!;
+        int numero = int.Parse(numeroString);
+        //Console.WriteLine(numero);
+        List<string> linguagens = Exercicio2().ToList();
+        if(linguagens.Count() < numero - 1){
+            Console.WriteLine("Não existe registro para o valor informado.");
+        } else {
+            Console.WriteLine(linguagens[numero - 1]); 
+        }
+        
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,71 @@
 ﻿
+using System.Runtime.CompilerServices;
+
 class Program{
     private static void Main(string[] args)
     {
-        string mensagemDeBoasVindas = "Seja Bem Vindo";
-        Console.WriteLine(mensagemDeBoasVindas);
-        string texto = Console.ReadLine();
-        Console.WriteLine("* {0} *", texto);
-        Console.WriteLine($"# {texto} #");
+
+        Exercicio exercicios = new Exercicio();
+        //exercicios.Exercicio1();
+        //exercicios.Exercicio2();
+        //exercicios.Exercicio3();
+        exercicios.Exercicio4();
+
+        //Screen Sound
+        string mensagemDeBoasVindas = @"
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░";
+
+        Program program = new Program();
+
+        program.ExibirMensagemDeBoasVinda(mensagemDeBoasVindas);
+        program.ExibirMensagemDeBoasVinda("\nBoas Vindas ao Screen Sound");
+        string opcao = program.ExibirOpcoesDoMenu();
+
+        program.Opcoes(opcao);
+
+    }
+
+    void ExibirMensagemDeBoasVinda(string mensagem){
+        Console.WriteLine(mensagem);
+    }
+
+    string ExibirOpcoesDoMenu(){
+        Console.WriteLine("\nDigite 1 para registrar uma banda");
+        Console.WriteLine("Digite 2 para mostrar todas as bandas");
+        Console.WriteLine("Digite 3 para avaliar uma banda");
+        Console.WriteLine("Digite 5 para sair");
+
+        Console.Write("\nDigite sua opção: ");
+        string opcaoEscolhida = Console.ReadLine()!;
+
+        return opcaoEscolhida;
+    }
+
+    void Opcoes(String opcao){
+        if(!int.TryParse(opcao, out int opcoes)){
+            Console.WriteLine("Valor digitado não é numérico.");
+        } else {
+            int opcaoNumerica = int.Parse(opcao);
+            string descricao = "Você digitou a opcao " + opcaoNumerica;
+            switch (opcaoNumerica){
+                case 1: Console.WriteLine(descricao); 
+                    break;
+                case 2: Console.WriteLine(descricao);
+                    break;
+                case 3: Console.WriteLine(descricao);
+                    break;
+                case 4: Console.WriteLine(descricao);
+                    break;
+                case 5: Console.WriteLine("Tchau Tchau :)");
+                    break;
+                default: Console.WriteLine("Opção inválida");
+                    break;
+            }
+        }
     }
 }
