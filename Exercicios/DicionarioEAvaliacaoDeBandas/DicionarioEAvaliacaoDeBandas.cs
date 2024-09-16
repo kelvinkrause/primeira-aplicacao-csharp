0a313154c08db547eb4479ecfddc77dc48b79391
kelvinkrause <kelvinrkrause@outlook.com>
2024-09-16 13:05:34 -0300

    modified:   Exercicios/DicionarioEAvaliacaoDeBandas/DicionarioEAvaliacaoDeBandas.cs 	modified:   bin/Debug/net6.0/primeiro-programa.dll 	modified:   bin/Debug/net6.0/primeiro-programa.exe 	modified:   bin/Debug/net6.0/primeiro-programa.pdb 	modified:   obj/Debug/net6.0/apphost.exe 	modified:   obj/Debug/net6.0/primeiro-programa.GeneratedMSBuildEditorConfig.editorconfig 	modified:   obj/Debug/net6.0/primeiro-programa.assets.cache 	modified:   obj/Debug/net6.0/primeiro-programa.csproj.CoreCompileInputs.cache 	modified:   obj/Debug/net6.0/primeiro-programa.dll 	modified:   obj/Debug/net6.0/primeiro-programa.genruntimeconfig.cache 	modified:   obj/Debug/net6.0/primeiro-programa.pdb 	modified:   obj/primeiro-programa.csproj.nuget.dgspec.json 	modified:   obj/primeiro-programa.csproj.nuget.g.props 	modified:   obj/project.assets.json 	modified:   obj/project.nuget.cache

--- Exercicios/DicionarioEAvaliacaoDeBandas/DicionarioEAvaliacaoDeBandas.cs
+++ Exercicios/DicionarioEAvaliacaoDeBandas/DicionarioEAvaliacaoDeBandas.cs
@@ -11,7 +11,8 @@ namespace primeiro_programa.Exercicios.DicionarioEAvaliacaoDeBandas
         public DicionarioEAvaliacaoDeBandas()
         {
             Exercicio1();
-
+            Exercicio2();
+            Exercicio3();
         }
 
         void Exercicio1()
@@ -37,6 +38,55 @@ namespace primeiro_programa.Exercicios.DicionarioEAvaliacaoDeBandas
                 }
             }
         }
+
+        void Exercicio2(){
+            /*
+            * Criar um programa que gerencie o estoque de uma loja. 
+            * Utilize um dicionário para armazenar produtos e suas quantidades em estoque e mostre, a partir do nome de um produto, sua quantidade em estoque.
+            */
+
+            Dictionary<string, int> produtos = new Dictionary<string, int>();
+
+            produtos.Add("Liquidificador", 10);
+            produtos.Add("Geladeira", 10);
+            produtos.Add("Fogão", 5);
+
+            Console.Write("Digite o nome de um produto para saber sua quantidade em estoque: ");
+            string nomeProduto = Console.ReadLine()!;
+            Console.WriteLine(produtos.ContainsKey(nomeProduto) ? $"Produto: {nomeProduto}, Qtd. Estoque: {produtos[nomeProduto].ToString()}" : "Produto não existe no cadastro.");
+
+        }
+
+        void Exercicio3(){
+            /*
+            * Crie um programa que implemente um quiz simples de perguntas e respostas. Utilize um dicionário para armazenar as perguntas e as respostas corretas.
+            */
+
+            Dictionary<string, string> perguntasERespostas = new Dictionary<string, string> {
+                { "Qual o nome da cidade onde o Kelvin mora? ", "Três Coroas" },
+                { "Qual é a idade do Kelvin? ", "27" },
+                {"Qual é o esporte atual do Kelvin? ", "Corrida" },
+            };
+
+            int pontuacao = 0;
+
+            Console.WriteLine("Bem vindo ao Quiz sobre o Kelvin Krause");
+
+            foreach (string pergunta in perguntasERespostas.Keys){
+                Console.WriteLine($"{pergunta}");
+                Console.Write("Digite sua responsta: ");
+                string responsta = Console.ReadLine()!;
+
+                if(perguntasERespostas[pergunta].ToLower().Equals(responsta.ToLower())){
+                    Console.WriteLine("Resposta Correta!\n");
+                    pontuacao++;
+                } else {
+                Console.WriteLine($"Resposta Incorreta. A resposta correta é: {perguntasERespostas[pergunta].ToString()} ;(");
+                }
+            }
+
+        }
+
 
     }
 }
